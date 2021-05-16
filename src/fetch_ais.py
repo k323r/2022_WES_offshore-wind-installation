12818a6aa661e9a24c8261ac3e85b9b2ba4457c4
Christian Backe <etzfxh@gmail.com>
2021-05-16 12:59:22 +0200

    Improve parameter names in fetch_ais (x -> lon, y -> lat).

--- src/fetch_ais.py
+++ src/fetch_ais.py
@@ -21,20 +21,19 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
 #URL = 'https://www.marinetraffic.com/legacy/getxml_i?sw_x=7.93&sw_y=52.07&ne_x=10.69&ne_y=53.83&zoom=14'
 URL_PREFIX = 'https://www.marinetraffic.com/legacy/getxml_i?'
+ZOOM = '&zoom=14'
 
-def fetch_ais(sw_x=7.93, sw_y=52.07, ne_x=10.69, ne_y=53.83):
-    response = requests.get(
-        f'{URL_PREFIX}sw_x={sw_x}&sw_y={sw_y}&ne_x={ne_x}&ne_y={ne_y}&zoom=14',
-        headers=HEADERS)
+def fetch_ais(sw_lon=7.93, sw_lat=52.07, ne_lon=10.69, ne_lat=53.83):
+    fmt = f'utc_%Y-%m-%d-%H-%M-%S_pos_{sw_lon}-{sw_lat}-{ne_lon}-{ne_lat}.xml'
+    position = f'sw_x={sw_lon}&sw_y={sw_lat}&ne_x={ne_lon}&ne_y={ne_lat}'
+    url = f'{URL_PREFIX}{position}{ZOOM}'
+    response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
         utcnow = datetime.datetime.utcnow()
         dir_data = os.path.join(
             DIR_DATA_AIS, 'raw', utcnow.strftime('utc_%Y-%m-%d'))
         os.makedirs(dir_data, exist_ok=True)
-        path_data = os.path.join(
-            dir_data,
-            utcnow.strftime(
-                f'utc_%Y-%m-%d-%H-%M-%S_pos_{sw_x}-{sw_y}-{ne_x}-{ne_y}.xml'))
+        path_data = os.path.join(dir_data, utcnow.strftime(fmt))
         with open(path_data, 'w') as f:
             print(response.text, file=f)
 
