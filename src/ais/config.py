3321cae87ca297e7defc8f4c02257e51069e052e
Aljoscha Sander <aljoscha.sander@mailbox.org>
2022-08-28 13:28:00 +0200

    Revert "Updates from Overleaf"
    This reverts commit e3edca06077c28a2e967cecd39b2a56a58bc3499.

---
+++ src/ais/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DIR_DATA = os.path.join(ROOT, 'data')
+DIR_DATA_AIS = os.path.join(DIR_DATA, 'ais')
+#DIR_PLOTS = os.path.join(ROOT, 'plots')
+#DIR_RESULTS = os.path.join(ROOT, 'results')
+#DIR_REPORTS = os.path.join(ROOT, 'reports')
+DIR_SRC = os.path.join(ROOT, 'src')
+#PATH_MPLSTYLE = os.path.join(DIR_SRC, 'style.mplstyle')
+#PATH_MPLSTYLE_PUBLICATION = os.path.join(DIR_SRC, 'style_publication.mplstyle')
